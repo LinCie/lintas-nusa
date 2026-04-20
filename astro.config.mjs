afa929107bbee57bf5c23a9532372cf5c0a47b32
LinCie <aldiofernanda@protonmail.com>
2026-04-21 00:25:24 +0700

    feat: add proper seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://lintasnusa.lincie.me",
 	server: {
 		host: true,
 		port: 4321,
